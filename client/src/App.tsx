e4f32e2d7f111e99185b4fa7f8286b6cfdb23836
Pasha Khoshkebari <pkhoshkeba@smail.pembinatrails.ca>
2022-12-27 12:09:42 -0600

    Fetch from Client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  return <div>Hello</div>
+  const [data, setData] = useState<any>()
+  useEffect(() => {
+    const getData = async () => {
+      await fetch('http://localhost:4000/hello')
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data)
+          setData(data)
+        })
+        .catch((error) => console.log(error))
+    }
+    getData()
+  }, [])
+
+  return (
+    <div>
+      <button onClick={() => console.log(data)}>PRINT</button>
+    </div>
+  )
 }
 
 export default App
