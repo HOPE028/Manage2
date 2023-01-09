3d70d76ddaef771e206f9685b145f35bcd56f244
Pasha Khoshkebari <pkhoshkeba@smail.pembinatrails.ca>
2023-01-08 20:06:22 -0600

    Update: Fetching required user info successfully

--- client/src/Components/Manager/InputInformationManager.tsx
+++ client/src/Components/Manager/InputInformationManager.tsx
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import {
+  interfaceCustomUserInfoRequired,
+  interfaceSpecialUserInfoRequired,
+} from './TeamSettings'
 
 export default function InputInformationManager() {
+  const [customUserInfoRequired, setCustomUserInfoRequired] = useState<
+    Array<interfaceCustomUserInfoRequired>
+  >([])
+  const [specialUserInfoRequired, setSpecialUserInfoRequired] = useState<
+    Array<interfaceSpecialUserInfoRequired>
+  >([])
+
   const getTeamRequiredInfo = async (UID: String) => {
     axios
       .get('http://localhost:4000/api/get_user_requirements_through_uid', {
         params: { userUID: UID },
       })
-      .then((res) => console.log(res))
+      .then((res) => res.data.data)
+      .then((res) => {
+        setCustomUserInfoRequired(res.Custom_User_Info_Required)
+        setSpecialUserInfoRequired(res.Special_User_Info_Required)
+      })
       .catch((error) => console.log(error))
   }
 
@@ -23,5 +38,28 @@ export default function InputInformationManager() {
     getUserUID()
   }, [])
 
-  return <div>InputInformationManager</div>
+  return (
+    <div>
+      <h1>Input the information</h1>
+      <h3>All information is required!</h3>
+
+      {customUserInfoRequired &&
+        customUserInfoRequired.map((field, index) => {
+          return <div key={index}>{field.name}</div>
+        })}
+
+      {/* {specialUserInfoRequired &&
+        specialUserInfoRequired.map((field, index) => {
+          return <div key={index}>{field.name}</div>
+        })} */}
+
+      <button
+        onClick={() =>
+          console.log(customUserInfoRequired, specialUserInfoRequired)
+        }
+      >
+        Data
+      </button>
+    </div>
+  )
 }
