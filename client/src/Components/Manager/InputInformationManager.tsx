7863eb03854a505baa8b30dfb5bf8fc65cff5c54
Pasha Khoshkebari <pkhoshkeba@smail.pembinatrails.ca>
2023-01-14 23:04:37 -0600

    Input Manager Prepared

--- client/src/Components/Manager/InputInformationManager.tsx
+++ client/src/Components/Manager/InputInformationManager.tsx
@@ -4,6 +4,12 @@ import {
   interfaceCustomUserInfoRequired,
   interfaceSpecialUserInfoRequired,
 } from './TeamSettings'
+import './ChoosingTeamSettings/style.css'
+import { v4 as uuidv4 } from 'uuid'
+
+interface dynamicObject {
+  [key: string]: String | Number | Boolean
+}
 
 export default function InputInformationManager() {
   const [customUserInfoRequired, setCustomUserInfoRequired] = useState<
@@ -13,10 +19,11 @@ export default function InputInformationManager() {
     Array<interfaceSpecialUserInfoRequired>
   >([])
 
-  // interface userInfo {
-  //   oo
-  // }
-  const [userInfo, setUserInfo] = useState({})
+  const [userInfo, setUserInfo] = useState<dynamicObject>({})
+
+  useEffect(() => {
+    getUserUID()
+  }, [])
 
   const getUserUID = async () => {
     axios
@@ -47,52 +54,132 @@ export default function InputInformationManager() {
     customInfoRequired: Array<interfaceCustomUserInfoRequired>,
     specialInfoRequired: Array<interfaceSpecialUserInfoRequired>
   ) => {
-    let tempUserInput = {}
+    let tempUserInput: dynamicObject = {}
     for (let field of customInfoRequired) {
-      console.log(field.name)
+      if (['String', 'LongString', 'File'].includes(field.valueType)) {
+        tempUserInput[field.name] = ''
+      } else if (field.valueType === 'Number') {
+        tempUserInput[field.name] = 0
+      } else if (field.valueType === 'Boolean') {
+        tempUserInput[field.name] = false
+      }
     }
+    for (let field of specialInfoRequired) {
+      if (['String', 'LongString', 'File'].includes(field.valueType)) {
+        tempUserInput[field.name] = ''
+      } else if (field.valueType === 'Number') {
+        tempUserInput[field.name] = 0
+      } else if (field.valueType === 'Boolean') {
+        tempUserInput[field.name] = false
+      }
+    }
+    setUserInfo(tempUserInput)
   }
-
-  const handleUserInfoChange = () => {}
-
-  useEffect(() => {
-    getUserUID()
-  }, [])
 
   return (
     <div>
       <h1>Input the information</h1>
       <h3>All information is required!</h3>
+      <InputInformation
+        userInfoRequired={customUserInfoRequired}
+        userInfo={userInfo}
+        setUserInfo={setUserInfo}
+      />
 
-      {customUserInfoRequired &&
-        customUserInfoRequired.map((field, index) => {
-          if (['String', 'LongString', 'Number'].includes(field.valueType)) {
-            return (
-              <div key={index}>
-                <h3>{field.name}</h3>
-                <h4>{field.description}</h4>
-                {field.valueType === 'Number' ? (
-                  <input type='number' />
-                ) : (
-                  <input type='text' />
-                )}
-              </div>
-            )
-          }
+      <InputInformation
+        userInfoRequired={specialUserInfoRequired}
+        userInfo={userInfo}
+        setUserInfo={setUserInfo}
+      />
+      <button onClick={() => console.log(userInfo)}>Data</button>
+    </div>
+  )
+}
+
+interface interfaceInputInformation {
+  userInfoRequired:
+    | Array<interfaceCustomUserInfoRequired>
+    | Array<interfaceSpecialUserInfoRequired>
+  userInfo: dynamicObject
+  setUserInfo: Function
+}
+
+const InputInformation = (props: interfaceInputInformation) => {
+  const handleUserInfoChange = (
+    key: string,
+    target: String | Number | Boolean
+  ) => {
+    let temp = props.userInfo
+    temp[key] = target
+    props.setUserInfo(temp)
+  }
+
+  return (
+    <div>
+      {props.userInfoRequired &&
+        props.userInfoRequired.map((field, index) => {
+          const key = uuidv4()
+          return (
+            <div key={index}>
+              <h3>{field.name}</h3>
+              <h4>{field.description}</h4>
+
+              {field.valueType === 'String' && (
+                <div>
+                  <input
+                    type='text'
+                    onChange={(e) =>
+                      handleUserInfoChange(field.name, e.target.value)
+                    }
+                    maxLength={30}
+                  />
+                </div>
+              )}
+              {field.valueType === 'Number' && (
+                <div>
+                  <input
+                    type='number'
+                    onChange={(e) =>
+                      handleUserInfoChange(field.name, Number(e.target.value))
+                    }
+                    maxLength={30}
+                  />
+                </div>
+              )}
+              {field.valueType === 'LongString' && (
+                <div>
+                  <textarea
+                    onChange={(e) =>
+                      handleUserInfoChange(field.name, e.target.value)
+                    }
+                  />
+                </div>
+              )}
+              {field.valueType === 'Boolean' && (
+                <div>
+                  <input
+                    type='checkbox'
+                    id={key}
+                    className='toggle'
+                    onChange={(e) => {
+                      handleUserInfoChange(
+                        field.name,
+                        Boolean(e.target.checked)
+                      )
+                      console.log(Boolean(e.target.checked))
+                    }}
+                  />
+                  <label htmlFor={key}></label>
+                </div>
+              )}
+              {field.valueType === 'File' && (
+                <div>
+                  <input type='file' />
+                </div>
+              )}
+            </div>
+          )
         })}
-
-      {/* {specialUserInfoRequired &&
-        specialUserInfoRequired.map((field, index) => {
-          return <div key={index}>{field.name}</div>
-        })} */}
-
-      <button
-        onClick={() =>
-          console.log(customUserInfoRequired, specialUserInfoRequired)
-        }
-      >
-        Data
-      </button>
     </div>
   )
 }
