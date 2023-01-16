cc4d4b553deb3eeddf8581acdfc56ff5720bb8c6
Pasha Khoshkebari <pkhoshkeba@smail.pembinatrails.ca>
2023-01-15 20:11:41 -0600

    Reducing unneccesary text

--- server/firebase/firebase-config.js
+++ server/firebase/firebase-config.js
@@ -2,13 +2,16 @@ const { firebaseConfig } = require('./config')
 const { initializeApp } = require('firebase/app')
 const { getFirestore } = require('firebase/firestore')
 const { getAuth } = require('firebase/auth')
+const { getStorage } = require('firebase/storage')
 
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 const auth = getAuth(app)
+const storage = getStorage(app)
 
 module.exports = {
   app,
   db,
   auth,
+  storage,
 }
